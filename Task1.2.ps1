7cdf751b6e038b46b4fbb02a17745abc61cd2304
Andrii Korneichuk <Andrii_Korneichuk@epam.com>
2018-07-30 18:28:41 +0300

    task 2&3 with exceptinos

--- Task1.2.ps1
+++ Task1.2.ps1
@@ -1,26 +1,32 @@
 #Creating parameters for script
 Param (
-   $width,
-   $lenght
+    $width,
+    $lenght
 )
 #Creating object with parameters
 try {
-$Square=$width*$lenght
+    $Square = $width * $lenght
     $rect = @{
-        wid=$width;
-        len=$lenght;
-        square=$Square
+        wid    = $width;
+        len    = $lenght;
+        square = $Square
     }
-#    Write-Host $rect.square
-    #Write-Host $S
     #Write out parameters of object
-          if (($rect.square -gt 0) -and ($rect.square.GetType() -ne [string] ))
-  {
-             Write-Host "Square = " $rect.square }
-             else {
-                Write-Host "Input parameters should be positive integer numbers" # String or non-positive parameters exception
-             }
+    if (($rect.square -gt 0) -and ($rect.square.GetType() -ne [string] )) {
+        Write-Host "Square = "$rect.square
+    }
+    else {
+        throw [System.ArgumentException]::new("Invalid input data")
+    }
+    # Write-Host "Square = " $rect.square }
+    # else {
+    #    Write-Error "Input parameters should be positive integer numbers" # String or non-positive parameters exception
+    # }
+}
+catch [System.ArgumentException] {
+    Write-Error $_.exception.Tostring()
 }
 catch {
-    Write-Host "Input parameters should be positive integer numbers"
+    Write-Error "Input parameters should be positive integer numbers"
 }
+exit
--- Task1.3.ps1
+++ Task1.3.ps1
@@ -4,23 +4,27 @@ Param (
     $lenght
 )
 # Cheching out if reqctangle is tiny or huge and writing out result.
-try{
-     $square = $lenght*$width
-     if ($square -le 100) {
-         if ($square -gt 0) {
+try {
+    $square = $lenght * $width
+    if ($square -le 100) {
+        if ($square -gt 0) {
             Write-Host "Square = "$square
             Write-Host "Your rectangle is tiny"
-         }
-         else {
-             Write-Host "Input parameters should be positive numbers" # Non positive parameters exception
-         }
+        }
+        else {
+            throw [System.ArgumentException]
+        }
     }
     else {
-            Write-Host "Square = "$square
-            Write-Host "Your rectangle is huge"
-         }
+        Write-Host "Square = "$square
+        Write-Host "Your rectangle is huge"
+    }
 }
 # Exception for invalid format of input data
-    catch {
-        Write-Host "You assigned lenght or width in invalid format. This parameters should be positive numbers"
-    }
+catch [System.ArgumentException] {
+    Write-Error "Input parameters should be positive numbers" # Non positive parameters exception
+}
+catch {
+    Write-Error "You assigned lenght or width in invalid format. This parameters should be positive numbers"
+}
+Exit
